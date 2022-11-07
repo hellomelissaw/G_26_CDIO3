5ece4c514f45ee3cc0a6cbb1b89880ded36bcb83
NegarOstad <s224283@student.dtu.dk>
2022-11-07 19:12:18 +0100

    Add GuiControllor class

--- src/GameComponents/DeedSquare.java
+++ src/GameComponents/DeedSquare.java
@@ -1,5 +1,24 @@
 package GameComponents;
 
-public class DeedSquare {
+public class DeedSquare extends SquareName{
 
+    Deed deed;
+    boolean sellDeed = false;
+
+    public DeedSquare(String deedName , Deed deed) {
+        super(deedName);
+        this.deed = deed;
+    }
+
+    Deed getDeed(){
+        return deed;
+    }
+
+    public boolean hasDeed(){ // Checks if the square has a deed available to buy or if it's already sold
+        return sellDeed;
+    }
+
+    public void sellDeed(){ // Sets deed to null so Square no longer has a deed
+        sellDeed = true ;
+    }
 }
