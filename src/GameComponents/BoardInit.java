e743667efeb2cbc7b86d97e0e8d0ac0654dc0152
NegarOstad <s224283@student.dtu.dk>
2022-11-07 19:08:14 +0100

    Implemente class BoardInit

--- src/GameComponents/BoardInit.java
+++ src/GameComponents/BoardInit.java
@@ -1,4 +1,11 @@
 package GameComponents;
 
 public class BoardInit {
+    public void initializeDeeds(){
+        Deed[] boardDeed = new Deed[24];
+
+        boardDeed[0] = new Deed(2, "Start");
+
+
+    }
 }
